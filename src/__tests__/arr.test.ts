b933f669827f9407e43acc305765ed8f7a3a1831
Roman Ekzemplyarov <rexe@ya.ru>
2021-09-15 20:30:50 +0300

    change index to main

--- src/__tests__/arr.test.ts
+++ src/__tests__/arr.test.ts
@@ -1,4 +1,4 @@
-import { arr, str } from '../index';
+import { arr, str } from '../main';
 
 test('arr', () => {
   expect(arr(str())).toStrictEqual({ type: 'array', items: { type: 'string' } });
--- src/__tests__/str.test.ts
+++ src/__tests__/str.test.ts
@@ -1,4 +1,4 @@
-import { str } from '../index';
+import { str } from '../main';
 
 test('str', () => {
   expect(str()).toStrictEqual({ type: 'string' });
